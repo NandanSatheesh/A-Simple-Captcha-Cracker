29673d56e3d3f995385010d772594b8c82f122b0
Nandan Satheesh <nandan.199776@gmail.com>
2018-06-11 18:38:23 +0530

    Delete p4.py

--- p4.py
+++
@@ -1,8 +0,0 @@
-from datetime import date
-from datetime import time
-from datetime import datetime
-from datetime import timedelta
-
-print(timedelta(days=365,hours=8 ,minutes=15))
-print("today is "+str(datetime.now()))
-print("one year from now:" + str(datetime.now())+timedelta(days=365)))
